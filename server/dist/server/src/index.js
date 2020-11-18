0f0cbe950b1223fd0fd5ccfd0ac277190c5f9e7c
Dysiewicz <joshua.dysiewicz@fujitsu.com>
2020-11-18 16:29:49 +0000

    Hopefully fixed the socket io issue

--- server/dist/server/src/index.js
+++ server/dist/server/src/index.js
@@ -15,9 +15,9 @@ app.use(function (req, res, next) {
 });
 app.use(cors());
 app.options('*', cors());
-app.use(express_1.default.static(path_1.default.join(__dirname, '../../../build')));
+app.use(express_1.default.static(path_1.default.join(__dirname, '../../build')));
 app.get('*', (req, res) => {
-    res.sendFile(path_1.default.join(__dirname, "../../../build", "index.html"));
+    res.sendFile(path_1.default.join(__dirname, "../../build", "index.html"));
 });
 exports.default = app;
 //# sourceMappingURL=index.js.map
